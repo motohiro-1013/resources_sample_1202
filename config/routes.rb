06753c9756db8451ca9f82e1e999469b512f99f9
motohiro-1013 <mo37to@gmail.com>
2019-12-02 07:16:13 +0000

    add create resources app

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,3 @@
 Rails.application.routes.draw do
-    
-    # 一覧画面へのルーティング
-    get "/users", to:"users#index"
-    
-    # 新規投稿画面へのルーティング
-    get "/users/new", to:"users#new"
-    
-    # 新規登録のためのルーティング
-    post "/users", to: "users#create"
-    
-    # 編集
-    get "/users/:id/edit", to: "users#edit"
-    
-    # 更新
-    patch "/users/:id", to: "users#update"
-    
-    # 削除
-    delete "/users/:id", to:"users#destroy"
+   resources :users
 end
